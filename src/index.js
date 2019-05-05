8fb12b1f2b7aa012dee591a9c045777e228ebfc2
sergey <sramator@gmail.com>
2019-05-05 10:48:16 +0300

    Add props and proptypes

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import React from 'react';
 import {render} from 'react-dom';
 import App from './components/App';
 
-render(<App />, document.getElementById(`root`));
+const titles = [`Beautiful & luxurious apartment at great location`, `Wood and stone place`, `Canal View Prinsengracht`, `Nice, cozy, warm big bed apartment`];
+
+render(<App data={titles} />, document.getElementById(`root`));
